280ddff511307d0b4d375e43f665cb12da316bab
Carlos <cgomespimentel7@gmail.com>
2024-11-16 19:44:20 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Watch Health')
-    .setDescription('Watch Health API description')
+    .setDescription('Watch Health API')
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
