81f5664e58579a694f562250262cd7f5cafe5951
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2024-11-17 11:33:01 -0300

    Store and fetch news from database

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { NewsService } from './news.service';
+import { Response } from 'express'; // Import Response type to handle the response
 
 @Controller('news')
 export class NewsController {
-  constructor(private readonly newsService: NewsService) {}
+  constructor(private readonly newsService: NewsService) { }
 
+  // Route to get news from the database
   @Get()
-  async getNews() {
-    return this.newsService.fetchNews();
+  async getNews(@Res() res: Response) {
+    try {
+      // Fetch news from the database
+      const news = await this.newsService.getAllNews();
+      return res.status(HttpStatus.OK).json(news);
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error fetching news', error });
+    }
+  }
+
+  // Route to trigger a database update by fetching new posts and saving them
+  @Get('update')
+  async updateNews(@Res() res: Response) {
+    try {
+      // Trigger the fetch and update process
+      const news = await this.newsService.fetchNews();
+      this.newsService.createNewsPosts(news);
+      return res.status(HttpStatus.OK).json({ message: 'News database updated successfully' });
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ message: 'Error updating news', error });
+    }
   }
 }
 
--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { NewsController } from './news.controller';
 import { NewsService } from './news.service';
 import { HttpModule } from '@nestjs/axios';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, PrismaModule],
   controllers: [NewsController],
   providers: [NewsService],
 })
-export class NewsModule {}
+export class NewsModule { }
 
