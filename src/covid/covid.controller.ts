bea746e85cb34a6b7b7ee2f29b76ff012cdec66c
Carlos <cgomespimentel7@gmail.com>
2024-11-17 13:05:49 -0300

    refactor: Documentacao das rotas

--- src/covid/covid.controller.ts
+++ src/covid/covid.controller.ts
@@ -1,28 +1,36 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CovidService } from './covid.service';
+import { ApiOperation, ApiResponse, ApiParam, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Covid')
 @Controller('covid')
 export class CovidController {
-    constructor(private readonly covidService: CovidService) {}
+  constructor(private readonly covidService: CovidService) {}
 
-
-    @Get('estado/:uf')
-    async getCovidDataByState(@Param('uf') uf: string) {
-      try {
-        const data = await this.covidService.getCovidData(uf);
-        return data; 
-      } catch (error) {
-        return { error: 'Erro ao obter os dados de COVID' };
-      }
+  @Get('estado/:uf')
+  @ApiOperation({ summary: 'Obtém dados de COVID-19 por estado' })
+  @ApiParam({ name: 'uf', required: true, description: 'UF do estado (ex: SP, RJ)' })
+  @ApiResponse({ status: 200, description: 'Dados de COVID-19 do estado', type: Object })
+  @ApiResponse({ status: 400, description: 'Erro ao obter os dados de COVID' })
+  async getCovidDataByState(@Param('uf') uf: string) {
+    try {
+      const data = await this.covidService.getCovidData(uf);
+      return data; 
+    } catch (error) {
+      return { error: 'Erro ao obter os dados de COVID' };
     }
+  }
 
-    @Get('teste')
-    async sendCovidNewsletter() {
-      try {
-        const data = await this.covidService.sendCovidNewsletter();
-        return data; 
-      } catch (error) {
-        return { error: 'Erro ao obter os dados de COVID' };
-      }
+  @Get('teste')
+  @ApiOperation({ summary: 'Envia a newsletter de COVID-19' })
+  @ApiResponse({ status: 200, description: 'Newsletter de COVID-19 enviada com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Erro ao enviar a newsletter' })
+  async sendCovidNewsletter() {
+    try {
+      const data = await this.covidService.sendCovidNewsletter();
+      return data; 
+    } catch (error) {
+      return { error: 'Erro ao enviar a newsletter' };
     }
+  }
 }
--- src/rss/rss.controller.ts
+++ src/rss/rss.controller.ts
@@ -3,33 +3,50 @@ import { DengueService } from 'src/dengue/dengue.service';
 import { RssService } from './rss.service';
 import { CovidService } from 'src/covid/covid.service';
 import { LegislationService } from 'src/legislation/legislation.service';
+import { ApiTags, ApiOperation, ApiQuery } from '@nestjs/swagger';
 
+@ApiTags('RSS')  // Adiciona a tag para agrupar as rotas no Swagger
 @Controller('rss')
 export class RssController {
-    constructor(private readonly dengueService: DengueService,private readonly rssService: RssService,private readonly covidService: CovidService,private readonly legislationService:LegislationService) {}
+  constructor(
+    private readonly dengueService: DengueService,
+    private readonly rssService: RssService,
+    private readonly covidService: CovidService,
+    private readonly legislationService: LegislationService
+  ) {}
 
-    @Get('covid')
-    async getRssCovidData(@Query('ibgecode') ibgecode: string) {
-      const data = await this.covidService.getCovidData(ibgecode);
-  
-      return this.rssService.getRssCovidData([data]);
-    }
+  @Get('covid')
+  @ApiOperation({ summary: 'Obtém dados RSS sobre a Covid-19' })  
+  @ApiQuery({ name: 'uf', required: true, description: 'Código UF do município' })  
+  async getRssCovidData(@Query('uf') uf: string) {
+    const data = await this.covidService.getCovidData(uf);
+    return this.rssService.getRssCovidData([data]);
+  }
 
-    @Get('dengue')
-    async getRssDengueData(@Query('ibgecode') ibgecode: string, @Query('disease') disease: string) {
-      const data = await this.dengueService.getDengueData(ibgecode, disease);
-      
-      return this.rssService.getRssDengueData(data);
-    }
+  @Get('dengue')
+  @ApiOperation({ summary: 'Obtém dados RSS sobre a Dengue' })  
+  @ApiQuery({ name: 'ibgecode', required: true, description: 'Código IBGE do município' })  
+  @ApiQuery({ name: 'disease', required: true, description: 'Nome da doença (por exemplo, dengue)' })  
+  async getRssDengueData(
+    @Query('ibgecode') ibgecode: string,
+    @Query('disease') disease: string
+  ) {
+    const data = await this.dengueService.getDengueData(ibgecode, disease);
+    return this.rssService.getRssDengueData(data);
+  }
 
-    @Get('legislation')
-    async getRssLegislationData(
-        @Query('tipo') tipo: string,
-        @Query('ano') ano: number,
-        @Query('from') from: string | null
-    ) {
-        const fromDate = from ? new Date(from) : null;
-        const data = await this.legislationService.getLegislation(tipo, ano, fromDate);
-        return this.rssService.getRssLegislationData(data);
-    }
+  @Get('legislation')
+  @ApiOperation({ summary: 'Obtém dados RSS sobre a Legislação' })  
+  @ApiQuery({ name: 'tipo', required: true, description: 'Tipo da legislação' })  
+  @ApiQuery({ name: 'ano', required: true, description: 'Ano da legislação' })  
+  @ApiQuery({ name: 'from', required: false, description: 'Data de início (formato: yyyy-mm-dd)' })  
+  async getRssLegislationData(
+    @Query('tipo') tipo: string,
+    @Query('ano') ano: number,
+    @Query('from') from: string | null
+  ) {
+    const fromDate = from ? new Date(from) : null;
+    const data = await this.legislationService.getLegislation(tipo, ano, fromDate);
+    return this.rssService.getRssLegislationData(data);
+  }
 }
